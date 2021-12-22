ca837ebb851aee7d7c1587bdf82960792d6d7fda
walle9 <308821845@qq.com>
2021-12-22 15:50:50 +0800

    添加导航

--- app/src/main/java/com/walle/playandroid/MainActivity.kt
+++ app/src/main/java/com/walle/playandroid/MainActivity.kt
@@ -2,10 +2,29 @@ package com.walle.playandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.walle.playandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+                R.id.mineFragment
+            )
+        )
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
     }
 }
